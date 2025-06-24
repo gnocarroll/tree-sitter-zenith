841b1ed6c8fdde389bf9fe95e1ddefa8961cd32c
gnocarroll <gocarrol@purdue.edu>
2025-06-23 22:41:58 -0500

    good work on grammar, have a lot of expression parsing #EPIC

--- grammar.js
+++ grammar.js
@@ -9,8 +9,16 @@
 
 const LINE_SEP = "\n";
 
+const B_EXPR = ($, prec_no, operator) => prec.left(prec_no, seq(
+  field("lhs", $.expr),
+  field("op", operator),
+  field("rhs", $.expr),
+));
+
 module.exports = grammar({
   name: "zenith",
+
+  word: $ => $.identifier,
 
   rules: {
     // TODO: add the actual grammar rules
@@ -18,22 +26,36 @@ module.exports = grammar({
 
     _definition: $ => choice(
       $.function_definition,
-      $.type_definition,
     ),
 
     function_definition: $ => seq(
       "function",
-      $.identifier,
+      field("name", $.identifier),
+      field("parameters", $.parameter_list),
+      optional(seq( // return type (can give var name)
+        "=>",
+        field("returnSpec", choice(
+          $.type,
+          $.pattern_and_type,
+        )),
+      )),
+      LINE_SEP,
+      repeat(seq($._statement, LINE_SEP)),
+      "end",
+      field("endName", $.identifier),
+    ),
+
+    parameter_list: $ => seq(
       "(",
       $.optional_newlines,
         // zero or more args followed by comma
-        repeat(
+        repeat(prec.right(
           seq(
             $.create_instance,
             ",",
             $.optional_newlines,
           )
-        ),
+        )),
 
         // for last arg separate it out so comma can
         // be optional
@@ -45,16 +67,6 @@ module.exports = grammar({
           )
         ),
       ")",
-      optional(seq( // return type (can give var name)
-        "=>",
-        choice(
-          $.type,
-          $.pattern_and_type,
-        ),
-      )),
-      repeat($._statement),
-      "end",
-      $.identifier,
     ),
 
     // e.g. A : T = B or A := B
@@ -64,26 +76,100 @@ module.exports = grammar({
     ),
 
     pattern_and_type: $ => seq(
-      $.pattern,
+      field("pattern", $.pattern),
       ":",
-      $.type,
+      field("type", $.type),
     ),
 
     expr: $ => choice(
-
+      $.primary_expr,
+      $.unary_expr,
+      $.binary_expr,
+      $.postfix_expr,
     ),
+
+    primary_expr: $ => choice(
+      $.identifier_w_namespace,
+      $.float_literal,
+      $.integer_literal,
+      seq("(", $.expr, ")"),
+    ),
+
+    unary_expr: $ => choice(
+      prec.right(10,seq(
+        field("op", choice(
+          "+", "-",
+          "not",
+          "~",
+        )),
+        field("subExpr", $.expr),
+      )),
+    ),
+
+    binary_expr: $ => choice(
+      // logical or, and
+      B_EXPR($, 1, "or"),
+      B_EXPR($, 2, "and"),
+      
+      // comparison operators
+      B_EXPR($, 3, choice("==", "!=")),
+      B_EXPR($, 4, choice(
+        "<", "<=", ">", ">="
+      )),
+
+      // bitwise binary operators
+      B_EXPR($, 5, "|"),
+      B_EXPR($, 6, "^"),
+      B_EXPR($, 7, "&"),
+
+      // addition and subtraction, then mult/div/mod
+      B_EXPR($, 8, choice(
+        "+", "-",
+      )),
+      B_EXPR($, 9, choice(
+        "*", "/", "%",
+      )),
+    ),
+
+    postfix_expr: $ => prec.left(11, seq(
+      field("lhs", $.expr),
+      choice(
+        seq(".", $.identifier), // member access
+        seq("[", $.expr, "]"), // array access
+        $.function_call_args,
+      )
+    )),
+
+    function_call_args: $ => seq(
+      "(",
+      seq(
+        // parser will not enforce args before kwargs,
+        // that wil come later in processing
+        repeat(prec.right(seq($.arg_or_kwarg, ","))),
+        optional(seq($.arg_or_kwarg, optional(","))),
+      ),
+      ")",
+    ),
+
+    arg_or_kwarg: $ => choice(
+      $.expr,
+      $.kwarg,
+    ),
+
+    kwarg: $ => seq($.pattern, "=", $.expr),
 
     type: $ => choice(
       $.identifier_w_namespace,
     ),
 
     _statement: $ => choice(
+      $.create_instance,
       $.inc_dec_statement,
     ),
 
     inc_dec_statement: $ => seq(
       $.identifier,
-      choice($.inc, $.dec),
+      choice("++", "--"),
     ),
 
     pattern: $ => choice (
@@ -99,12 +185,11 @@ module.exports = grammar({
       $.identifier,
     ),
 
-    optional_newlines: $ => optional(seq(LINE_SEP)),
-
-    inc: $ => "++",
-    dec: $ => "--",
+    optional_newlines: $ => token(repeat(LINE_SEP)),
 
     identifier: $ => /[A-Za-z_][A-Za-z0-9_]*/,
-    digits: $ => /\d+/,
+
+    float_literal: $ => /(\d+\.\d*|\d*\.\d+)([Ee]\d+)?/,
+    integer_literal: $ => /\d+/,
   }
 });
